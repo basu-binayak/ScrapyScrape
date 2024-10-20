85ad44f41d60aa64117eda6583482c58056bf0fa
Binayak Basu <basubinayak05@hotmail.com>
2024-10-20 12:52:51 +0530

    new commit

--- a_ebook_scraper/ebook_scraper/ebook_scraper/items.py
+++ a_ebook_scraper/ebook_scraper/ebook_scraper/items.py
@@ -3,11 +3,35 @@
 # See documentation in:
 # https://docs.scrapy.org/en/latest/topics/items.html
 
-from scrapy import Item, Field
+# from scrapy import Item, Field
 
 
-class EbookItem(Item):
-    title = Field()         # To store the book title
-    rating = Field()        # To store the rating (e.g., 'Three', 'Four')
-    price = Field()         # To store the price of the ebook
-    stock_status = Field()  # To store the stock availability
+# class EbookItem(Item):
+#     title = Field()         # To store the book title
+#     rating = Field()        # To store the rating (e.g., 'Three', 'Four')
+#     price = Field()         # To store the price of the ebook
+#     stock_status = Field()  # To store the stock availability
+
+# items.py
+import scrapy
+from scrapy.loader.processors import MapCompose, TakeFirst, Join
+
+# Define the Item class and associate processors for each field
+class EbookItem(scrapy.Item):
+    title = scrapy.Field(
+        input_processor=MapCompose(lambda x: x.strip()),  # Strip leading/trailing whitespace
+        # output_processor = TakeFirst()
+    )
+    rating = scrapy.Field(
+        input_processor=MapCompose(lambda x: x.split(' ')[1]),  # Extract rating from class attribute
+        # output_processor = TakeFirst()
+    )
+    price = scrapy.Field(
+        input_processor=MapCompose(lambda x: x.replace('£', '').strip(),
+                                   lambda x: float(x)),  # Remove currency symbol and strip
+        # output_processor = TakeFirst()
+    )
+    stock_status = scrapy.Field(
+        input_processor = MapCompose(lambda x: ''.join(x).strip()),
+        # output_processor = TakeFirst()
+    )
